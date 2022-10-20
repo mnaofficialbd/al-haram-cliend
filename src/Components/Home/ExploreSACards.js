f1d43f7bb76ef421c4a10eafdf3c4441884a13ca
mnaofficial <mna.official92@gmail.com>
2022-10-20 14:46:15 +0600

    exploreSA text setup

--- src/Components/Home/ExploreSACards.js
+++ src/Components/Home/ExploreSACards.js
@@ -6,12 +6,12 @@ import riyadhImg from '../../Assets/ExploreSA/riyadh.png'
 
 const ExploreSACards = () => {
     return (
-        <section className='container mb-4 py-2'>
+        <section className='container mb-4 py-2 text-white'>
             <div class="row row-cols-1 row-cols-md-4 g-4">
                 <div class="col">
-                    <div class="card h-100">
+                    <div class="card h-100 ">
                         <img src={makkahImg} class="card-img" alt="..." />
-                        <div className="card-img-overlay justify-content-center align-items-end">
+                        <div className="card-img-overlay text-center">
                             <h2 className='text-uppercase'>Makkah</h2>
                             <p className='text-uppercase'>The holy city of prophet muhammad (pbuh)</p>
                         </div>
@@ -20,7 +20,7 @@ const ExploreSACards = () => {
                 <div class="col">
                     <div class="card h-100">
                         <img src={madinaImg} class="card-img" alt="..." />
-                        <div className="card-img-overlay justify-content-center align-items-end">
+                        <div className="card-img-overlay text-center">
                             <h2 className='text-uppercase'>Madina</h2>
                             <p className='text-uppercase'>The holy city of prophet muhammad (pbuh)</p>
                         </div>
@@ -29,7 +29,7 @@ const ExploreSACards = () => {
                 <div class="col">
                     <div class="card h-100">
                         <img src={jeddahImg} class="card-img" alt="..." />
-                        <div className="card-img-overlay justify-content-center align-items-end">
+                        <div className="card-img-overlay text-center">
                             <h2 className='text-uppercase'>Jeddah</h2>
                             <p className='text-uppercase'>The holy city of prophet muhammad (pbuh)</p>
                         </div>
@@ -38,7 +38,7 @@ const ExploreSACards = () => {
                 <div class="col">
                     <div class="card h-100">
                         <img src={riyadhImg} class="card-img" alt="..." />
-                        <div className="card-img-overlay justify-content-center align-items-end">
+                        <div className="card-img-overlay text-center">
                             <h2 className='text-uppercase'>Riyadh</h2>
                             <p className='text-uppercase'>The holy city of prophet muhammad (pbuh)</p>
                         </div>
