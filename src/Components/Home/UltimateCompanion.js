5e2879d702b0f6f089a0682cdb5ac8fa64ac99bc
mnaofficial <mna.official92@gmail.com>
2022-10-21 23:00:35 +0600

    ultimate campanion primary setup

---
+++ src/Components/Home/UltimateCompanion.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const UltimateCompanion = () => {
+    return (
+        <section className=' container p-2'>
+            <div className="row">
+                <div className="col-6">
+                    <div className="row">
+                        <div className="col-12"></div>
+                        <div className="row">
+                            <div className="col-6">
+                                <div className="col-2"></div>
+                                <div className="col-2"></div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="col-6">
+
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default UltimateCompanion;
