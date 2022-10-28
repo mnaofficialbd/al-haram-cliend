60d9bcc595a445a7798b782068083782cedd1131
mnaofficial <mna.official92@gmail.com>
2022-10-28 09:38:23 +0600

    hero summery section create

--- src/Components/Home/Hero.js
+++ src/Components/Home/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import bgImg from '../../Assets/Backgrounds/alHaramTopBennerBg.png';
 import HeroSearchSection from './HeroSearchSection';
+import HeroSummerySection from './HeroSummerySection';
 
 const Hero = () => {
     return (
@@ -15,49 +16,8 @@ const Hero = () => {
                     {/* -------select/search section------------ */}
                     <HeroSearchSection />
 
-
                     {/* summery part */}
-                    <div className="row g-4 text-white">
-                        <div className="col-lg-3 col-12 col-md-6 ">
-                            <div className=''>
-                                <img src="https://i.ibb.co/nkWZkJg/verify-icon.png" className="mx-auto d-block" alt="..." />
-                            </div>
-                            <div className='text-center'>
-                                <h3>1500+</h3>
-                                <h3>VERIFIED AGENTS</h3>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-12 col-md-6">
-                            <div className=''>
-                                <img src="https://i.ibb.co/zZ5xjq3/happy-client.png" className="mx-auto d-block" alt="..." />
-                            </div>
-                            <div className='text-center'>
-                                <h3>1 LAKH+</h3>
-                                <h3>HAPPY GUESTS</h3>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-12 col-md-6">
-                            <div className=''>
-                                <img src="https://i.ibb.co/7bNCkqD/booking-icon.png" className="mx-auto d-block" alt="..." />
-                            </div>
-                            <div className='text-center'>
-                                <h3>2 LAKH+</h3>
-                                <h3>BOOKINGS DONE</h3>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-12 col-md-6">
-                            <div className=''>
-                                <img src="https://i.ibb.co/pRt0XrG/package-icon.png" className="mx-auto d-block" alt="..." />
-                            </div>
-                            <div className='text-center'>
-                                <h3>1K+</h3>
-                                <h3>PACKAGES</h3>
-                            </div>
-                        </div>
-
-
-
-                    </div>
+                    <HeroSummerySection />
                 </div>
             </div>
         </section>
---
+++ src/Components/Home/HeroSummerySection.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const HeroSummerySection = () => {
+    return (
+        <div className="row g-4 text-white">
+            <div className="col-lg-3 col-12 col-md-6 ">
+                <div className=''>
+                    <img src="https://i.ibb.co/nkWZkJg/verify-icon.png" className="mx-auto d-block" alt="..." />
+                </div>
+                <div className='text-center'>
+                    <h3>1500+</h3>
+                    <h3>VERIFIED AGENTS</h3>
+                </div>
+            </div>
+            <div className="col-lg-3 col-12 col-md-6">
+                <div className=''>
+                    <img src="https://i.ibb.co/zZ5xjq3/happy-client.png" className="mx-auto d-block" alt="..." />
+                </div>
+                <div className='text-center'>
+                    <h3>1 LAKH+</h3>
+                    <h3>HAPPY GUESTS</h3>
+                </div>
+            </div>
+            <div className="col-lg-3 col-12 col-md-6">
+                <div className=''>
+                    <img src="https://i.ibb.co/7bNCkqD/booking-icon.png" className="mx-auto d-block" alt="..." />
+                </div>
+                <div className='text-center'>
+                    <h3>2 LAKH+</h3>
+                    <h3>BOOKINGS DONE</h3>
+                </div>
+            </div>
+            <div className="col-lg-3 col-12 col-md-6">
+                <div className=''>
+                    <img src="https://i.ibb.co/pRt0XrG/package-icon.png" className="mx-auto d-block" alt="..." />
+                </div>
+                <div className='text-center'>
+                    <h3>1K+</h3>
+                    <h3>PACKAGES</h3>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HeroSummerySection;
