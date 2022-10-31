43269cbe03f4b63ccb9512dfc45aca2e69c999c9
mnaofficial <mna.official92@gmail.com>
2022-10-31 10:51:51 +0600

    special offer section setup complete

--- src/Components/Home/SpecialOffer.js
+++ src/Components/Home/SpecialOffer.js
@@ -1,44 +1,48 @@
 import React from 'react';
-import makkahImg from '../';
-import planeImg from '../';
-import roomImg from '../';
+import makkahImg from '../../Assets/SpecialOffer/mokka.png';
+import planeImg from '../../Assets/SpecialOffer/plane.png';
+import roomImg from '../../Assets/SpecialOffer/room.png';
 
 const SpecialOffer = () => {
     return (
         <section className='container'>
             <div className="text-center my-5">
-                <h3 className='text-uppercase' style={{ fontSize:'40px'}}>Special Offers</h3>
-                <p className="text-capitalize" style={{color: '#777777', fontSize:'24px'}}>These popular destinations have a lot to offer</p>
+                <h3 className='text-uppercase' style={{ fontSize: '40px' }}>Special Offers</h3>
+                <p className="text-capitalize" style={{ color: '#777777', fontSize: '24px' }}>These popular destinations have a lot to offer</p>
             </div>
             <div className="row row-cols-1 row-cols-md-3 g-4">
                 <div className="col">
                     <div className="card h-100 ">
                         <img src={makkahImg} className="card-img" alt="..." />
-                        <div className="card-img-overlay text-center">
-                            <h2 className='text-uppercase'>Makkah</h2>
-                            <p className='text-uppercase'>The holy city of prophet muhammad (pbuh)</p>
+                        <div className="card-img-overlay">
+                            <h2 className='text-uppercase text-white'>Explore Umrah Packages</h2>
+                            <button type='button' className='btn btn-light text-uppercase px-4 rounded-0 mt-2'>View More</button>
                         </div>
                     </div>
                 </div>
                 <div className="col">
                     <div className="card h-100">
                         <img src={planeImg} className="card-img" alt="..." />
-                        <div className="card-img-overlay text-center">
-                            <h2 className='text-uppercase'>Madina</h2>
-                            <p className='text-uppercase'>The holy city of prophet muhammad (pbuh)</p>
+                        <div className="card-img-overlay">
+                            <h2 className='text-uppercase text-white'>Create your own umrah package</h2>
+                            <button type='button' className='btn btn-light text-uppercase px-4 rounded-0 mt-2'>View More</button>
                         </div>
                     </div>
                 </div>
                 <div className="col">
                     <div className="card h-100">
                         <img src={roomImg} className="card-img" alt="..." />
-                        <div className="card-img-overlay text-center">
-                            <h2 className='text-uppercase'>Jeddah</h2>
-                            <p className='text-uppercase'>The holy city of prophet muhammad (pbuh)</p>
+                        <div className="card-img-overlay">
+                            <h2 className='text-uppercase text-white'>
+                                Up to 70%
+                                <br />
+                                Discount!
+                            </h2>
+                            <button type='button' className='btn btn-light text-uppercase px-4 rounded-0 mt-2'>View More</button>
                         </div>
                     </div>
                 </div>
-                
+
             </div>
         </section>
     );
