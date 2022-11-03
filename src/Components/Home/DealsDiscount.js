f7ff4cd018b071ff2b8e15f7d555f16e9d34e283
mnaofficial <mna.official92@gmail.com>
2022-11-03 11:07:42 +0600

    deals and discounts esction design

--- src/Components/Home/DealsDiscount.js
+++ src/Components/Home/DealsDiscount.js
@@ -6,7 +6,7 @@ const DealsDiscount = () => {
         <section className='container mt-5 pt-5'>
             <div className="d-md-flex flex-column justify-content-center align-items-center">
                 <div className='text-center'>
-                    <h3 className='text-uppercase font-semibold' style={{fontSize:'40px'}}>Popular umrah deals</h3>
+                    <h3 className='text-uppercase font-semibold' style={{fontSize:'40px'}}>Deals & Diswcounts</h3>
                     <p style={{fontSize:'24px', color:'#777777'}}>Trending Umrah Packages From Popular Destinations</p>
                 </div>
                 <div>
