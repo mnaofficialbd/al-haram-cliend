fac3b6529c0ee81214411b6df8cc572464fc7cef
mnaofficial <mna.official92@gmail.com>
2022-11-09 11:00:53 +0600

    hero summery section map system apply

--- src/Components/Home/HeroSummerySection.js
+++ src/Components/Home/HeroSummerySection.js
@@ -5,46 +5,31 @@ import bookingIcon from '../../Assets/SummaryIcon/bookingVector.png';
 import packIcon from '../../Assets/SummaryIcon/packageVector.png';
 
 const HeroSummerySection = () => {
+
+    const datas = [
+        { id: 1, title: 'VERIFIED AGENTS', subTitle: '1500+', img: verifyIcon },
+        { id: 2, title: 'HAPPY GUESTS', subTitle: '1 LAKH+', img: guestIcon },
+        { id: 3, title: 'BOOKINGS DONE', subTitle: '2 LAKH+', img: bookingIcon },
+        { id: 3, title: 'PACKAGES', subTitle: '1K +', img: packIcon }
+    ]
+
     return (
         <section style={{ backgroundColor: '#02A3C1' }}>
             <div className="container">
                 <div className="row g-4 text-white">
-                    <div className="col-lg-3 col-12 col-md-6 ">
-                        <div className=''>
-                            <img src={verifyIcon} className="mx-auto d-block" alt="..." />
-                        </div>
-                        <div className='text-center'>
-                            <h3>1500+</h3>
-                            <h3>VERIFIED AGENTS</h3>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-12 col-md-6">
-                        <div className=''>
-                            <img src={guestIcon} className="mx-auto d-block" alt="..." />
-                        </div>
-                        <div className='text-center'>
-                            <h3>1 LAKH+</h3>
-                            <h3>HAPPY GUESTS</h3>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-12 col-md-6">
-                        <div className=''>
-                            <img src={bookingIcon} className="mx-auto d-block" alt="..." />
-                        </div>
-                        <div className='text-center'>
-                            <h3>2 LAKH+</h3>
-                            <h3>BOOKINGS DONE</h3>
-                        </div>
-                    </div>
-                    <div className="col-lg-3 col-12 col-md-6">
-                        <div className=''>
-                            <img src={packIcon} className="mx-auto d-block" alt="..." />
-                        </div>
-                        <div className='text-center'>
-                            <h3>1K +</h3>
-                            <h3>PACKAGES</h3>
-                        </div>
-                    </div>
+                    {
+                        datas.map(data => (
+                            <div key={data.id} className="col-lg-3 col-12 col-md-6 ">
+                                <div className=''>
+                                    <img src={data.img} className="mx-auto d-block" alt="..." />
+                                </div>
+                                <div className='text-center'>
+                                    <h3>{data.subTitle}</h3>
+                                    <h3>{data.title}</h3>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         </section>
