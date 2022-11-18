8a7424f07686ef172d5d9ad2687943fe0f7611d2
mnaofficial <mna.official92@gmail.com>
2022-11-18 18:37:30 +0600

    slide and sidebar section setup modify

--- src/Components/PackageDetails/SlideSidebar.js
+++ src/Components/PackageDetails/SlideSidebar.js
@@ -1,4 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
@@ -6,24 +5,13 @@ import slideImg from '../../Assets/packageDetails/slideImg.png'
 
 const SlideSidebar = () => {
 
-    const countryDatas = [
-        { id: 1, title: 'Egypt', num: 58 },
-        { id: 2, title: 'India', num: 69 },
-        { id: 3, title: 'Pakistan', num: 95 },
-        { id: 4, title: 'Bangladesh', num: 65 },
-        { id: 5, title: 'Malaysia', num: 35 },
-        { id: 6, title: 'Indonesia', num: 48 },
-        { id: 7, title: 'Turkey', num: 56 },
-        { id: 8, title: 'Azerbaijan', num: 37 },
-        { id: 9, title: 'Afghanistan', num: 25 },
-    ]
 
     return (
         <section className='mt-5 pt-5 mx-4 details_slide_section'>
 
             <div className="details_slide px-5">
-                <div className="border border-dark d-flex justify-content-center">
-                    <Swiper navigation={true}  modules={[Navigation]} className="px-5">
+                <div className="mb-3 d-flex justify-content-center">
+                    <Swiper navigation={true} modules={[Navigation]} className="px-5">
                         <div className=''>
                             <SwiperSlide>
                                 <div>
@@ -43,33 +31,55 @@ const SlideSidebar = () => {
 
             <div class="d-flex flex-column flex-shrink-0 px-2 details_sidebar" style={{ background: '#02A3C1' }} >
                 <div className='rounded px-2 h-100'>
+                    <div className='my-5 mx-5'>
+                        <div className='px-3' >
+                            <div>
+                                <label for="formFile" class='mb-1 font-bold text-white' style={{ fontSize: '20px' }}>Departure City</label>
+                                <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" style={{ color: '#757575', fontSize: '20px' , width: '520px', height: '58px' }}>
+                                    <option selected>Ex Mumbai</option>
+                                    <option value="1">Kolkata</option>
+                                    <option value="2">Hydrebad</option>
+                                    <option value="3">Dilhi</option>
+                                </select>
+                            </div>
+                            <div>
+                                <label for="formFile" class='mb-1 font-bold text-white' style={{ fontSize: '20px' }}>Departure Date</label>
+                                <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example" style={{ color: '#757575', fontSize: '20px' , width: '520px', height: '58px' }}>
+                                    <option selected>11 Oct 2022</option>
+                                    <option value="1">12 Oct 2022</option>
+                                    <option value="2">13 Oct 2022</option>
+                                    <option value="3">14 Oct 2022</option>
+                                </select>
+                            </div>
+                            <div>
+                                <label for="formFile" class='mb-1 font-bold text-white' style={{ fontSize: '20px' }}>Package Category</label>
+                                <select class="mt-2 form-select form-select-lg mb-3" aria-label=".form-select-lg example" style={{ color: '#757575', fontSize: '20px' ,width: '520px', height: '58px' }}>
+                                    <option selected>Luxury</option>
+                                    <option value="1">Simple</option>
+                                    <option value="2">VIP</option>
+                                </select>
+                            </div>
+                            <div className='bg-white d-flex justify-content-between mb-3 px-4 align-items-center rounded' style={{ width: '520px', height: '58px' }}>
+                                <div>
+                                    <p style={{ color: '#757575', fontSize: '20px' }}>Per Person</p>
+                                </div>
+                                <div>
+                                    <h4 style={{ color: '#02A3C1', fontSize: '26px' }}>₹ 18000</h4>
+                                </div>
+                            </div>
+                            <div className='mt-3'>
+                                <button className='package_details_btn font-bold'>
+                                    Make An Enquiry
+                                </button>
+                            </div>
+                            <div className='mt-3'>
+                                <button className='package_details_btn font-bold'>
+                                    Book Now
+                                </button>
+                            </div>
 
-                    {/* =============Departure country section=============== */}
-                    <div className='border border-white m-5'>
-                        <div className='p-3 d-flex justify-content-between' >
-                            <div>
-                                <h6 className='font-bold text-white' style={{ fontSize: '20px' }}>Departure Country</h6>
-                            </div>
-                            <div>
-                                <FontAwesomeIcon icon="fa-regular fa-chevron-down" />
-                                {/* <FontAwesomeIcon icon={faChevronDown} /> */}
-                            </div>
                         </div>
-                        <div className='mt-3'>
-                            {
-                                countryDatas.map(countryData => (
-                                    <div key={countryData.id} className="d-flex justify-content-between px-4 font-semibold">
-                                        <div class="form-check form-check-inline">
-                                            <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" />
-                                            <label class="form-check-label" for="inlineCheckbox1">{countryData.title}</label>
-                                        </div>
-                                        <div>
-                                            <p>({countryData.num})</p>
-                                        </div>
-                                    </div>
-                                ))
-                            }
-                        </div>
+
                     </div>
 
 
