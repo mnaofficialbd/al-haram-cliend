a8f38aad38827f088352287d1bc6c0482515d7e8
mnaofficial <mna.official92@gmail.com>
2022-11-23 16:50:06 +0600

    hotel_search_section setup

--- src/Components/Hotel/HotelSearchSection.js
+++ src/Components/Hotel/HotelSearchSection.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { faCalendarDays, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const HotelSearchSection = () => {
     return (
@@ -6,8 +8,57 @@ const HotelSearchSection = () => {
             <div className='text-center py-4'>
                 <h6>Book Domestic And International Hotel Online</h6>
             </div>
-            <div>
+            <div className='py-4'>
+                <form>
+                    <div className='d-md-flex justify-content-center'>
 
+                        <div className='width_17 bg-white mb-2 me-3 me-lg-1 p-1'>
+                            <div className="d-flex justify-content-between">
+                                <p className="text-uppercase mb-2">City of location</p>
+                                <FontAwesomeIcon style={{ width: "18px", height: "18px" }} icon={faLocationDot}></FontAwesomeIcon>
+                            </div>
+                            <div className='ps-2'>
+                                <p className='text-uppercase font-semibold m-0' style={{ fontSize: '18px' }}>Makkah</p>
+                            </div>
+                        </div>
+
+                        <div className='width_17 mb-2 bg-white me-3 me-lg-1 p-1'>
+                            <div className="d-flex justify-content-between">
+                                <p className="text-uppercase mb-2">Check In</p>
+                                <FontAwesomeIcon style={{ width: "18px", height: "18px" }} icon={faCalendarDays}></FontAwesomeIcon>
+                            </div>
+                            <div className='ps-2'>
+                                <p className='text-uppercase font-semibold m-0' style={{ fontSize: '18px' }}>5 Aug 22</p>
+                            </div>
+                        </div>
+
+                        <div className='width_17 mb-2 bg-white me-3 me-lg-1 p-1'>
+                            <div className="d-flex justify-content-between">
+                                <p className="text-uppercase mb-2">Check Out</p>
+                                <FontAwesomeIcon style={{ width: "18px", height: "18px" }} icon={faCalendarDays}></FontAwesomeIcon>
+                            </div>
+                            <div className='ps-2'>
+                                <p className='text-uppercase font-semibold m-0' style={{ fontSize: '18px' }}>11 Aug 22</p>
+                            </div>
+                        </div>
+
+                        <div className='width_10 bg-white mb-2 me-3 me-lg-1 p-1'>
+                            <p className="text-uppercase mb-2">Room</p>
+                            <div className='ps-2'>
+                                <p className='text-uppercase font-semibold m-0' style={{ fontSize: '18px' }}>02</p>
+                            </div>
+                        </div>
+
+                        <div className='width_17 bg-white me-3 mb-2 me-lg-1 p-1'>
+                            <p className="text-uppercase mb-2">Guest</p>
+                            <div className='ps-2'>
+                                <p className='text-uppercase font-semibold m-0' style={{ fontSize: '18px' }}>02 Adults</p>
+                            </div>
+                        </div>
+
+                        <button className="width_17 custom_btn mb-2 px-4 p-1" type="button">SEARCH</button>
+                    </div>
+                </form>
             </div>
         </section>
     );
--- src/Pages/Hotel.js
+++ src/Pages/Hotel.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import UmrahLogoTextHeader from '../Components/Umrah/UmrahLogoTextHeader';
+import HotelSearchSection from '../Components/Hotel/HotelSearchSection';
 
 const Hotel = () => {
     return (
         <>
             <UmrahLogoTextHeader />
+            <HotelSearchSection />
         </>
     );
 };
