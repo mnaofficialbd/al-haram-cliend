8f567563ad055e0744af4cd4a877a7113ad4b035
mnaofficial <mna.official92@gmail.com>
2022-11-25 17:11:26 +0600

    Terms_&_conditions component setup

--- src/Components/PackageDetails/PackageAllDetailsTab.js
+++ src/Components/PackageDetails/PackageAllDetailsTab.js
@@ -6,6 +6,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import OverviewSection from './OverviewSection'
 import InformationSection from './InformationSection';
+import TermsConditions from './TermsConditions';
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -75,7 +76,7 @@ export default function PackageAllDetailsTab() {
                 User Reviews section
             </TabPanel>
             <TabPanel value={value} index={5}>
-                Terms & Conditions section
+                <TermsConditions />
             </TabPanel>
         </Box>
     );
--- src/Components/PackageDetails/TermsConditions.js
+++ src/Components/PackageDetails/TermsConditions.js
@@ -2,9 +2,22 @@ import React from 'react';
 
 const TermsConditions = () => {
     return (
-        <div>
-            
-        </div>
+        <section className='shadow '>
+            <div className=' p-4 pt-0 mx-5'>
+                <div className=''>
+                    <div className='py-2'>
+                        <p className='mx-4 m-0 font-bold'>Terms & Conditions</p>
+                    </div>
+                    <div className='' style={{ color: '#757575',fontSize: '16px' }}>
+                        <li className=''>Currently Child Below 18 Not Allowed</li>
+                        <li className=''>Currently Child Below 18 Not Allowed</li>
+                        <li className=''>Currently Child Below 18 Not Allowed</li>
+                        <li className=''>Currently Child Below 18 Not Allowed</li>
+                    </div>
+                </div>
+                
+            </div>
+        </section>
     );
 };
 
