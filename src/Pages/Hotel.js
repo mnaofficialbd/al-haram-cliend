4f84f84bcefc5ebb17c3f7780ce69100ee09ca00
mnaofficial <mna.official92@gmail.com>
2022-12-02 10:22:16 +0600

    hotel_details section component create

--- src/Pages/Hotel.js
+++ src/Pages/Hotel.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import HotelDetails from '../Components/Hotel/HotelDetails';
 import HotelSearchSection from '../Components/Hotel/HotelSearchSection';
 
 const Hotel = () => {
     return (
         <>
-           <HotelSearchSection /> 
-
+            <HotelSearchSection />
+            <HotelDetails />
         </>
     );
 };
