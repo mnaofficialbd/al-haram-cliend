68cf15cecc9074cffb1b21a82b57f1f8e7633843
mnaofficial <mna.official92@gmail.com>
2022-12-02 17:22:56 +0600

    hotel_details page create

---
+++ src/Components/Hotel/HotelShowMoreBtn.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HotelShowMoreBtn = () => {
+    return (
+        <section className='mt-5 d-flex justify-content-center'>
+            <button className="popular-card-btn text-uppercase">
+                Show more
+            </button>
+        </section>
+    );
+};
+
+export default HotelShowMoreBtn;
--- src/Pages/Hotel.js
+++ src/Pages/Hotel.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HotelDetails from '../Components/Hotel/HotelDetails';
 import HotelSearchSection from '../Components/Hotel/HotelSearchSection';
+import HotelShowMoreBtn from '../Components/Hotel/HotelShowMoreBtn';
 
 const Hotel = () => {
     return (
         <>
             <HotelSearchSection />
             <HotelDetails />
+            <HotelShowMoreBtn />
         </>
     );
 };
