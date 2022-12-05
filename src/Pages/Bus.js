a01f8f9fcc25c7568de01026d014bc5dea2679fb
mnaofficial <mna.official92@gmail.com>
2022-12-05 12:12:32 +0600

    bus component sidebar setup complete

--- src/Pages/Bus.js
+++ src/Pages/Bus.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import BusMoreDetails from '../Components/Bus/BusMoreDetails';
 import BusSearchSection from '../Components/Bus/BusSearchSection';
 
 const Bus = () => {
     return (
         <>
             <BusSearchSection />
+            <BusMoreDetails />
         </>
     );
 };
